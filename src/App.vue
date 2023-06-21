c97cf75089d3785a2781ed5b0f4d9c0f5f1a06f0
Carlos7979 <carloscorredorwg@gmail.com>
2023-06-20 22:54:56 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+	import Header from './components/Header.vue'
+	import Info from './components/Info.vue'
+
+	export default {
+		components: {
+			Header,
+			Info,
+		},
+		data() {
+			return {}
+		},
+		created() {},
+		methods: {},
+		computed: {}
+	}
+</script>
+
+<template>
+	<Header></Header>
+	<div class="main">
+		<Info></Info>
+		<hr />
+	</div>
+</template>
+
+<style scoped>
+
+</style>
