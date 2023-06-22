e2159bb404aeac42023164c9e05f5e35693ce3eb
Carlos7979 <carloscorredorwg@gmail.com>
2023-06-21 20:22:28 -0400

    add Form component and button for change to an horizontal view in App

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,15 @@
     import Header from './components/Header.vue'
     import Info from './components/Info.vue'
     import Table from './components/Table.vue'
-
-    const rows = [
-        { row: ['Nombre', 'Correo', 'Número', 'Password', 'Ciudad'] },
-        { row: ['Carlos', 'carlos@email.com', 20, 123, 'Caracas'] }
-    ]
+    import Form from './components/Form.vue'
+    const rows = [{ row: ['Nombre', 'Correo', 'Número', 'Password', 'Ciudad', 'Idiomas', 'Color'] }]
 
     export default {
         components: {
             Header,
             Info,
-            Table
+            Table,
+            Form
         },
         data() {
             return {
@@ -21,27 +19,69 @@
                     'header-purple': false
                 },
                 rows,
-                rowStyle: ['background: white', 'background: gray', '; background: gold']
+                rowStyle: ['background: white', 'background: gray', '; background: gold'],
+                horizontalView: false
             }
         },
         created() {},
-        methods: {},
+        methods: {
+            handleSubmit(data) {
+                const row = [
+                    data.name,
+                    data.email,
+                    data.number,
+                    data.password,
+                    data.city,
+                    data.lang,
+                    data.color
+                ]
+                this.rows.push({ row })
+            },
+            handleClick() {
+                this.horizontalView = !this.horizontalView
+            }
+        },
         computed: {}
     }
 </script>
 
 <template>
-	<div class="main">
-		<Header></Header>
-        <Info></Info>
-        <hr />
-        <Table
-            :rows="rows"
-            :row-style="rowStyle"
-            table-title="Tabla 1"
-            :header-class="headerClass"
-        ></Table>
+    <div class="main">
+        <Header></Header>
+        <div :class="horizontalView ? 'horizontal' : ''">
+            <div>
+                <Info></Info>
+                <hr v-show="!horizontalView" />
+            </div>
+            <button
+                :class="`btn btn-secondary ${horizontalView ? ' button-h' : ' button'}`"
+                @click="handleClick"
+            >
+                Cambiar vista
+            </button>
+            <Form @submit="handleSubmit"></Form>
+            <Table
+                :rows="rows"
+                :row-style="rowStyle"
+                table-title="Datos recolectados"
+                :header-class="headerClass"
+            ></Table>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+    .horizontal {
+        display: flex;
+        justify-content: space-around;
+    }
+
+	.button-h {
+		height: fit-content;
+		margin: 10px 40px;
+	}
+
+	.button {
+		margin: 10px 45%;
+	}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,125 @@
+<script>
+    export default {
+        name: 'Form',
+        emits: ['submit'],
+        mixins: [],
+        components: {},
+        props: {},
+        data() {
+            return {
+                form: {
+                    name: '',
+                    email: '',
+                    number: '',
+                    password: '',
+                    city: '',
+                    lang: [],
+                    color: ''
+                },
+                cities: ['', 'Caracas', 'Buenos Aires', 'Bogotá']
+            }
+        },
+        created() {},
+        mounted() {},
+        methods: {
+            handleSubmit() {
+                this.$emit('submit', this.form)
+            }
+        },
+        computed: {},
+        watcher: {}
+    }
+</script>
+
+<template>
+    <div class="form-container">
+        <form action="" method="post" class="form">
+            <div class="input-group-text">
+                <label for="name">Nombre</label>
+                <input class="form-control" v-model.trim="form.name" type="text" name="name" id="name" />
+            </div>
+            <div class="input-group-text">
+                <label for="email">Correo</label>
+                <input
+                    class="form-control"
+                    v-model="form.email"
+                    type="email"
+                    name="email"
+                    id="email"
+                />
+            </div>
+            <div class="input-group-text">
+                <label for="number">Número</label>
+                <input
+                    class="form-control"
+                    v-model.number="form.number"
+                    type="number"
+                    name="number"
+                    id="number"
+                />
+            </div>
+            <div class="input-group-text">
+                <label for="password">Contraseña</label>
+                <input
+                    class="form-control"
+                    v-model="form.password"
+                    type="password"
+                    name="password"
+                    id="password"
+                />
+            </div>
+            <div class="input-group-text">
+                <label for="city">Ciudad</label>
+                <select class="custom-select select" v-model="form.city" id="city">
+                    <option v-for="(city, i) in cities" :key="`${i}-cities`" :value="city">
+                        {{ city }}
+                    </option>
+                </select>
+            </div>
+            <div class="input-group-text">
+                <label>Idiomas</label>
+                <div class="form-control">
+                    <label for="es">Español</label>
+                    <input type="checkbox" name="lang" id="es" value="es" v-model="form.lang" />
+                    <label for="en">Inglés</label>
+                    <input type="checkbox" name="lang" id="en" value="en" v-model="form.lang" />
+                </div>
+            </div>
+            <div class="input-group-text">
+				<label>Color</label>
+                <div class="form-control">
+					<label for="blue">Azul</label>
+					<input type="radio" name="color" id="blue" value="azul" v-model="form.color" />
+					<label for="green">Verde</label>
+					<input type="radio" name="color" id="green" value="verde" v-model="form.color" />
+				</div>
+            </div>
+            <input @click.prevent="handleSubmit" type="submit" value="Enviar" />
+			<div class="alert alert-danger"></div>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+    .form-container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .select {
+        width: 100%;
+    }
+
+    .input-group-text label {
+        width: 7rem;
+        text-align: end;
+        font-weight: 600;
+        margin-right: 5px;
+    }
+
+</style>
