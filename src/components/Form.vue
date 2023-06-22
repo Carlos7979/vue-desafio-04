8563254a0c88bfd4829561af512a3f88c6fb0fbc
Carlos7979 <carloscorredorwg@gmail.com>
2023-06-22 01:52:56 -0400

    add validations to Form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -16,6 +16,7 @@
                     lang: [],
                     color: ''
                 },
+                errors: [],
                 cities: ['', 'Caracas', 'Buenos Aires', 'Bogotá']
             }
         },
@@ -23,24 +24,102 @@
         mounted() {},
         methods: {
             handleSubmit() {
-                this.$emit('submit', this.form)
+                if (this.validate()) {
+                    this.$emit('submit', this.form)
+                    this.resetForm()
+                }
+            },
+            validate() {
+                this.errors = []
+                const form = this.form
+                for (const input in form) {
+                    if ((input === 'lang' && form[input].length === 0) || form[input] === '') {
+                        this.$refs[input].focus()
+                        this.$refs[input].classList.add('shadow')
+                        this.errors.push('Todos los campos son obligatorios')
+                        return false
+                    } else {
+                        this.$refs[input].classList.remove('shadow')
+                    }
+                }
+                if (!this.emailValidate(form.email)) {
+                    this.$refs['email'].focus()
+                    this.$refs['email'].classList.add('shadow')
+                    this.errors.push(`${form.email} no es un email válido`)
+                    return false
+                }
+                this.errors = []
+                return true
+            },
+            emailValidate(email) {
+                const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+                if (!re.test(String(email))) return false
+                return true
+            },
+            resetForm() {
+                this.form = {
+                    name: '',
+                    email: '',
+                    number: '',
+                    password: '',
+                    city: '',
+                    lang: [],
+                    color: ''
+                }
             }
         },
         computed: {},
-        watcher: {}
+        watch: {
+            'form.name'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['name'].classList.remove('shadow')
+            },
+            'form.email'(newValue, oldValue) {
+                if (newValue && !oldValue) {
+                    this.$refs['email'].classList.remove('shadow')
+                    return
+                }
+                if (this.emailValidate(newValue)) {
+                    this.$refs['email'].classList.remove('shadow')
+                    this.validate()
+                }
+            },
+            'form.number'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['number'].classList.remove('shadow')
+            },
+            'form.password'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['password'].classList.remove('shadow')
+            },
+            'form.city'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['city'].classList.remove('shadow')
+            },
+            'form.lang.length'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['lang'].classList.remove('shadow')
+            },
+            'form.color'(newValue, oldValue) {
+                if (newValue && !oldValue) this.$refs['color'].classList.remove('shadow')
+            }
+        }
     }
 </script>
 
 <template>
     <div class="form-container">
-        <form action="" method="post" class="form">
+        <form ref="form" action="" method="post" class="form">
             <div class="input-group-text">
                 <label for="name">Nombre</label>
-                <input class="form-control" v-model.trim="form.name" type="text" name="name" id="name" />
+                <input
+                    ref="name"
+                    class="form-control"
+                    v-model.trim="form.name"
+                    type="text"
+                    name="name"
+                    id="name"
+                />
             </div>
             <div class="input-group-text">
                 <label for="email">Correo</label>
                 <input
+                    ref="email"
                     class="form-control"
                     v-model="form.email"
                     type="email"
@@ -51,8 +130,9 @@
             <div class="input-group-text">
                 <label for="number">Número</label>
                 <input
+                    ref="number"
                     class="form-control"
-                    v-model.number="form.number"
+                    v-model.prevent="form.number"
                     type="number"
                     name="number"
                     id="number"
@@ -61,6 +141,7 @@
             <div class="input-group-text">
                 <label for="password">Contraseña</label>
                 <input
+                    ref="password"
                     class="form-control"
                     v-model="form.password"
                     type="password"
@@ -70,7 +151,7 @@
             </div>
             <div class="input-group-text">
                 <label for="city">Ciudad</label>
-                <select class="custom-select select" v-model="form.city" id="city">
+                <select ref="city" class="custom-select select" v-model="form.city" id="city">
                     <option v-for="(city, i) in cities" :key="`${i}-cities`" :value="city">
                         {{ city }}
                     </option>
@@ -78,7 +159,7 @@
             </div>
             <div class="input-group-text">
                 <label>Idiomas</label>
-                <div class="form-control">
+                <div ref="lang" class="form-control">
                     <label for="es">Español</label>
                     <input type="checkbox" name="lang" id="es" value="es" v-model="form.lang" />
                     <label for="en">Inglés</label>
@@ -86,16 +167,29 @@
                 </div>
             </div>
             <div class="input-group-text">
-				<label>Color</label>
-                <div class="form-control">
-					<label for="blue">Azul</label>
-					<input type="radio" name="color" id="blue" value="azul" v-model="form.color" />
-					<label for="green">Verde</label>
-					<input type="radio" name="color" id="green" value="verde" v-model="form.color" />
-				</div>
+                <label>Color</label>
+                <div ref="color" class="form-control">
+                    <label for="blue">Azul</label>
+                    <input type="radio" name="color" id="blue" value="azul" v-model="form.color" />
+                    <label for="green">Verde</label>
+                    <input
+                        type="radio"
+                        name="color"
+                        id="green"
+                        value="verde"
+                        v-model="form.color"
+                    />
+                </div>
             </div>
             <input @click.prevent="handleSubmit" type="submit" value="Enviar" />
-			<div class="alert alert-danger"></div>
+            <div
+                v-show="errors.length > 0"
+                class="alert alert-danger"
+                v-for="(error, i) in errors"
+                :key="`${i}-error`"
+            >
+                {{ error }}
+            </div>
         </form>
     </div>
 </template>
@@ -121,5 +215,7 @@
         font-weight: 600;
         margin-right: 5px;
     }
-
+    .shadow {
+        box-shadow: 1px 1px 1px 1px red !important;
+    }
 </style>
